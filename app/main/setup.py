445cddd2d63f4459a38b19c24c52e5d49153dd5a
孙广龙 <guanglong.sun@hand-china.com>
2018-07-20 17:23:12 +0800

    py2exe打包测试

---
+++ app/main/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin /env  python
+# coding:utf-8 
+"""
+@version:  2.7.14
+@author:  Long
+@file:  setup.py
+@time: 2018/7/20 16:27
+"""
+import py2exe
+from distutils.core import setup
+
+setup(console=["guif.py"])
